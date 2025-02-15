38e1542a832d3404bd72d060e3c2dfcfd6fbfbfc
LucasSilvestreTech <lucassilvestre517@gmail.com>
2025-02-15 12:22:24 -0300

    Delete planetas-android directory

--- planetas-android/.github.io-android_planets-app-main/lib/utils/validators.dart
+++
@@ -1,9 +0,0 @@
-class Validators {
-  static String? validateName(String? value) {
-    if (value == null || value.isEmpty) {
-      return 'Nome não pode estar vazio';
-    }
-    return null;
-  }
-}
-```
